543940a116fbf1b7b81333a03e07662ef204af0f
joongwon <sara970517@gmail.com>
2025-05-20 00:04:54 +0900

    조건문과 반복문 : if, else if, else

---
+++ src/IfElse.java
@@ -0,0 +1,11 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int temp = 3;
+
+        if (temp < 10) {
+            System.out.println("자켓을 입으세요.");
+        } else {
+            System.out.println("자켓을 입지 마세요.");
+        }
+    }
+}
