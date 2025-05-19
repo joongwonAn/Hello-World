dbff32a4f3456551ca6f32697eeae4ba89470aef
joongwon <sara970517@gmail.com>
2025-05-19 21:55:42 +0900

    250519. 변수 - 자료형

---
+++ src/Variables2.java
@@ -0,0 +1,23 @@
+public class Variables2 {
+    public static void main(String[] args) {
+        boolean myBoolean = true;
+        myBoolean = false;
+
+        int myInt = 123;
+
+        long myLong = 12345678910L;
+
+        double myDouble = 3.14;
+
+        String myString = "codeit!";
+
+        System.out.println("hello!");
+
+        String hello = "Hello, i'm ";
+        String dot = ".";
+
+        System.out.print(hello);
+        System.out.print(27);
+        System.out.println(dot);
+    }
+}
