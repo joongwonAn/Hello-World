71f9160ab7338e33316363d9087e6398df420476
joongwon <sara970517@gmail.com>
2025-05-19 21:56:07 +0900

    250519. 변수 - 실습: 자기소개서

---
+++ src/Variables3.java
@@ -0,0 +1,13 @@
+public class Variables3 {
+    public static void main(String[] args) {
+        String name = "코드잇";
+        int year = 2015;
+        int month = 7;
+        int day = 20;
+        boolean married = false;
+
+        System.out.println("이름: " + name);
+        System.out.println("생일: " + year + "년" + month + "월" + day + "일");
+        System.out.println("결혼: " + married);
+    }
+}
