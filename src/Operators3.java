27ae775a6536ad534b02876eab76cb0c1af32cdc
joongwon <sara970517@gmail.com>
2025-05-19 23:02:59 +0900

    변수 - 비교 연산자

---
+++ src/Operators3.java
@@ -0,0 +1,11 @@
+public class Operators3 {
+    public static void main(String[] args) {
+        System.out.println("비교 연산자");
+        System.out.println(3 + " > " + 2 + " : " + (3 > 2));
+        System.out.println(3 + " < " + 2 + " : " + (3 < 2));
+        System.out.println(3 + " >= " + 2 + " : " + (3 >= 2));
+        System.out.println(3 + " <= " + 2 + " : " + (3 <= 2));
+        System.out.println(3 + " == " + 2 + " : " + (3 == 2));
+        System.out.println(3 + " != " + 2 + " : " + (3 != 2));
+    }
+}
