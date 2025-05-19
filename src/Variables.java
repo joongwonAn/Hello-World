56e6bb94f93bf5bd9728c34ed450c53bbfdf63ce
joongwon <sara970517@gmail.com>
2025-05-19 21:55:22 +0900

    250519. 변수

---
+++ src/Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+    public static void main(String[] args) {
+        int age;
+        age = 27;
+
+        int num = 21;
+        age = 26;
+
+//        age = 22.6;
+        age = num;
+        age = age + 1;
+
+        System.out.print("Hello. i'm");
+        System.out.print(age);
+        System.out.println(".");
+    }
+}
