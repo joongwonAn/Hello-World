2fb9586f1613ba4028903dfe5406660b7546bae0
joongwon <sara970517@gmail.com>
2025-05-19 23:02:53 +0900

    변수 - 숫자 연산자

---
+++ src/Operators2.java
@@ -0,0 +1,13 @@
+public class Operators2 {
+    public static void main(String[] args) {
+        System.out.println("숫자 연산자");
+        int a = 9;
+        int b = 5;
+//        double b = 5;
+        System.out.println(a + " + " + b + " = " + (a + b));
+        System.out.println(a + " - " + b + " = " + (a - b));
+        System.out.println(a + " * " + b + " = " + (a * b));
+        System.out.println(a + " / " + b + " = " + (a / b));
+        System.out.println(a + " % " + b + " = " + (a % b));
+    }
+}
