26b91f85f4d57c41b160f04642fe908bbe1339a2
joongwon <sara970517@gmail.com>
2025-05-19 23:40:15 +0900

    변수 - 실습: 연산자와 형 변환

---
+++ src/TypeConversionCasting2.java
@@ -0,0 +1,11 @@
+public class TypeConversionCasting2 {
+    public static void main(String[] args) {
+        int koreanScore = 86;
+        int mathScore = 94;
+        int scienceScore = 87;
+        int computerScore = 100;
+
+        double average = (koreanScore + mathScore + scienceScore + computerScore) / 4.0;
+        System.out.println(average);
+    }
+}
